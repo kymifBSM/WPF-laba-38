01cfc17939678d6c4a7e827a30c5cef7073b3ead
SNOWIK1 <dimais444@mail.ru>
2023-10-10 11:31:52 +0700

    Ne znay kak no eto rabotaet

---
+++ Wpf-laba-38/Client.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Wpf_laba_38
+{
+    public class Client
+    {
+        private TcpClient client;
+        private Thread receiveThread;
+        public event Action<string> MessageReceived;
+        public void Start(string ipAddress, int port)
+        {
+            client = new TcpClient();
+            client.Connect(ipAddress, port);
+            receiveThread = new Thread(new ThreadStart(ReceiveMessages));
+            receiveThread.Start();
+        }
+        private void ReceiveMessages()
+        {
+            StreamReader reader = new StreamReader(client.GetStream());
+            string serverMessage;
+            while ((serverMessage = reader.ReadLine()) != null)
+            {
+                MessageReceived?.Invoke(serverMessage);
+            }
+            client.Close();
+        }
+        public void SendMessage(string message)
+        {
+            StreamWriter writer = new StreamWriter(client.GetStream()){ AutoFlush = true };
+            writer.WriteLine(message);
+        }
+    }
+}
---
+++ Wpf-laba-38/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_laba_38
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private TcpListener server;
+        private Thread listenThread;
+        private Client client;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            sendButton.Click += SendButton_Click;
+            messageTextBox.KeyDown += MessageTextBox_KeyDown;
+            StartListening();
+            client = new Client();
+            client.MessageReceived += Client_MessageReceived;
+        }
+
+        private void StartListening()
+        {
+            server = new TcpListener(IPAddress.Any, 8888);
+            server.Start();
+            listenThread = new Thread(new ThreadStart(ListenForClients));
+            listenThread.Start();
+        }
+        private void ListenForClients()
+        {
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+                Thread clientThread = new Thread(new
+                ParameterizedThreadStart(HandleClientCommunication));
+                clientThread.Start(client);
+            }
+        }
+        private void HandleClientCommunication(object client)
+        {
+            TcpClient tcpClient = (TcpClient)client;
+
+            StreamWriter writer = new StreamWriter(tcpClient.GetStream())
+            { AutoFlush = true };
+            StreamReader reader = new StreamReader(tcpClient.GetStream());
+            string clientMessage;
+            while ((clientMessage = reader.ReadLine()) != null)
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    chatHistoryListBox.Items.Add("Client: " + clientMessage);
+                });
+            }
+            tcpClient.Close();
+        }
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            string message = messageTextBox.Text;
+            SendMessage(message);
+            messageTextBox.Clear();
+        }
+        private void MessageTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                SendButton_Click(sender, e);
+            }
+        }
+
+        private void Client_MessageReceived(string message)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                chatHistoryListBox.Items.Add("Server: " + message);
+
+            });
+        }
+        private void SendMessage(string message)
+        {
+            chatHistoryListBox.Items.Add("Client: " + message);
+            client.SendMessage(message);
+        }
+    }
+}
